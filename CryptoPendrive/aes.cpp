e44fbde1a8aa101d482afa055a262d65a8841239
baton112 <baton112@op.pl>
2015-01-18 22:08:22 +0100

    up

--- CryptoPendrive/aes.cpp
+++ CryptoPendrive/aes.cpp
@@ -1,11 +1,41 @@
 #include "aes.h"
 
 
-aes::aes(void)
+aes::aes(BYTE *key, int keyLength)
 {
+	rows = keyLength/32;
+	for(int i = 0; i < rows; i++) //row 
+	{
+		keyArray[i] = new BYTE[4]; //new colums 
+	}
 }
 
 
 aes::~aes(void)
 {
+}
+
+void aes::getByteBlock(BYTE *block)
+{
+	for(int i = 0; i < 4; i++)//kolumna //colum
+	{
+		for(int j = 0; j < 4; j++)//wiesz //row 
+		{
+			stateArray[j][i] = block[i*4+j];
+		}
+	}
+	
+}
+
+void aes::getKey(BYTE *key)
+{
+	for(int i = 0; i < 4; i++)//kolumna //colum
+	{
+		for(int j = 0; j < 4; j++)//wiesz //row 
+		{
+			keyArray[j][i] = key[i*4+j];
+		}
+	}
+
+
 }
--- CryptoPendrive/aes.h
+++ CryptoPendrive/aes.h
@@ -1,8 +1,20 @@
 #pragma once
+#include <windows.h>
+
 class aes
 {
 public:
-	aes(void);
+	aes(BYTE *key, int keyLength);
 	~aes(void);
+	void getByteBlock(BYTE *block); // przekszta³ca 128 bitów wejscowych na blok do AES // 16 bajtów 
+	void getKey(BYTE *key);
+private:
+	BYTE stateArray[4][4]; // 128 bitowy blok danych do kodowania 
+	/// rzad // kolumna  
+	BYTE *keyArray[4];
+	void getKey(BYTE *key);
+	int rows; 
+
+
 };
 
